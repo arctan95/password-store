8d04241de70cdb1a9476a8bae0ea9c9efd27f22a
arctan95 <jaeqentan@gmail.com>
2024-10-14 23:15:50 +0800

    Add importing private gpg key instruction

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "(!) To use passwords, you must import gpg private key first"
+PRIVATE_KEY_PATH="$1"
+
+if [[ -z "$PRIVATE_KEY_PATH" ]]; then
+  echo "(!) To use passwords, you must import gpg private key first: gpg --import /path/to/your-sub-private-key.asc"
+else
+  gpg --import "$PRIVATE_KEY_PATH"
+fi
+
 cat "$(dirname $0)/README.md"
